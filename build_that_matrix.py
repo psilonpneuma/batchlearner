6a8cb3cfc25031a69b01ffe52195a4b76a69467a
psilonpneuma <chiara.semenzin@gmail.com>
2017-01-31 00:50:39 +0000

    Update build_that_matrix.py

--- build_that_matrix.py
+++ build_that_matrix.py
@@ -1,3 +1,16 @@
+##
+# Bayesian batch learner simulation. A single agent observes a starting proportion of 1s and
+# 0s (e.g. 8:2), infers a distribution over possible grammars (i.e. ratios) and selects a language
+# using one of 3 possible strategies - maximum a posteriori, sampling or averaging. The agent then
+# produces another 10 datapoints using the same language, over n runs - default = 10000 using either
+# maximisation, soft-maximisation or sampling. The code returns a graph of production frequencies of
+# variant x across runs (shown on the x-axis) from the input observation ratios x:y. A matrix is extracted
+# from the iterations and decomposed in its eigenvectors to calculate the matrix steady states.
+###
+
+
+
+
 import random as rnd
 import matplotlib
 import matplotlib.pyplot as plt
